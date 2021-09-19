c1151c85ad718c98012060f3efc3f2d3f719e3f7
Сулаков Антон <sulakov_as@magnit.ru>
2021-09-19 15:52:48 +0300

    homework 05 Library application initial commit

---
+++ homework-library/src/test/java/ru/otus/homeworklibrary/HomeworkLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.homeworklibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
