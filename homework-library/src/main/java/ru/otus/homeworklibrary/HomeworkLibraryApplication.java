8e7126f4302965a414bcd5b4548d1fd01f46a361
Сулаков Антон <sulakov_as@magnit.ru>
2021-09-20 19:05:40 +0300

    homework 05 Library application add liquibase

--- homework-library/src/main/java/ru/otus/homeworklibrary/HomeworkLibraryApplication.java
+++ homework-library/src/main/java/ru/otus/homeworklibrary/HomeworkLibraryApplication.java
@@ -3,11 +3,21 @@ package ru.otus.homeworklibrary;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.h2.tools.Console;
+
+import java.sql.SQLException;
+
 @SpringBootApplication
 public class HomeworkLibraryApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HomeworkLibraryApplication.class, args);
+
+        try {
+            Console.main(args);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 }
