12986f8371f377c81a98b2673bc568cc386ee135
Сулаков Антон <sulakov_as@magnit.ru>
2022-01-27 19:04:51 +0300

    Homework 14 Spring Integration initial commit

---
+++ homework-integration/src/test/java/ru/otus/homeworkintegration/HomeworkIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.homeworkintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
